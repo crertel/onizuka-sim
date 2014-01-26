dbc91496863c6db946b1aacc2273147329407326
Chris Ertel <crertel@fancybastard>
2014-01-25 18:19:18 -0600

    Add log4js.
    Add log4js.
    Change to use log4js.

--- sim.js
+++ sim.js
@@ -2,6 +2,9 @@ var config = require("./config.js");
 var express = require('express');
 var zmq = require('zmq');
 var sockjs = require("sockjs");
+var log4js = require('log4js');
+var log = log4js.getLogger();
+log.setLevel('WARN');
 
 var assetSub;
 var webServer;
@@ -11,7 +14,7 @@ var monitorSocket;
 function startAssetSub() {
     var assetServerAddress = "tcp://" + config.assetServerAddress + ":" + config.assetServerPort;
     assetSub = zmq.socket('sub');
-    console.log("Subscribed to asset server at %s", assetServerAddress);
+    log.info("Subscribed to asset server at %s", assetServerAddress);
     assetSub.on('message', function(msg) {
      try {
          var msgObject = JSON.parse(msg);
@@ -21,31 +24,31 @@ function startAssetSub() {
                  conn.write(msgObjectString);
              });
          } catch (error) {
-             console.warn("Unable to send to operator: ", error.toString());
+             log.warn("Unable to send to operator: ", error.toString());
          }
-         console.info('Received message: ');
-         console.info( msgObjectString );
+         log.debug('Received message: ');
+         log.debug( msgObjectString );
      } catch ( error ) {
-         console.warn('Received bad message: \n%s', msg.toString());
+         log.warn('Received bad message: \n%s', msg.toString());
      }
 
     });
     assetSub.subscribe('');
     assetSub.connect(assetServerAddress);
 
-    console.info("done.");
+    log.info("done.");
 };
 
 var operators = [];
 
 function startMonitor(){
-    console.info("Starting monitor...");
+    log.info("Starting monitor...");
     monitorSocket = sockjs.createServer();
     monitorSocket.on('connection', function(conn) {
         operators.push(conn);
-        console.log("Operator joined.");
+        log.log("Operator joined.");
         conn.on('data', function(message) {
-            console.info("Operator says %s", message);
+            log.info("Operator says %s", message);
         });
         conn.on('close', function () {
             var opIndex;
@@ -53,28 +56,28 @@ function startMonitor(){
             if (opIndex != -1) {
                 operators.splice(opIndex,1);
             }
-            console.info("Operator left.");
+            log.info("Operator left.");
         });
     });
     monitorSocket.on('close', function(conn) {
     });
     monitorSocket.installHandlers(expressWebServer, {prefix:'/console'});
-    console.info("done.");
+    log.info("done.");
 };
 
 function startWeb() {
     var port = config.webPort;
-    console.info("Starting sim web server on port %s...", port);
+    log.info("Starting sim web server on port %s...", port);
     webServer = express();
 
     webServer.use(express.static(__dirname + '/web'));
     expressWebServer = webServer.listen(port);
-    console.info("done.");
+    log.info("done.");
 };
 
 function showConfig() {
-    console.info("Using config:");
-    console.info(config);
+    log.info("Using config:");
+    log.info(config);
 };
 
 function init() {
@@ -87,7 +90,7 @@ function init() {
 };
 
 function shutdown() {
-    console.warn("Shutting down...");
+    log.warn("Shutting down...");
     assetSub.close();
 };
 
